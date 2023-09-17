ac326e93a00e70bff86eea02463ce070dc2b610d
probiss <bulut.cihad@gmail.com>
2023-09-17 19:15:20 +0300

    static field updated

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static('public'));
 
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
