a2018c789379cc6394a15dd01f0396eaed07d4a4
probiss <bulut.cihad@gmail.com>
2023-09-19 10:02:02 +0300

    last touch.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
